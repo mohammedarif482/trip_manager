68ecbbb2415111e14d659effac57143479164c3c
arifrealtynow <mohammed.arif@realtynow.in>
2024-10-22 19:22:40 +0530

    report screen

--- lib/View/reports_screen.dart
+++ lib/View/reports_screen.dart
@@ -17,92 +17,82 @@ class ReportsScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Card(
-                color: AppColors.primaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Text(
-                        "Truck Revenue\nReport",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: AppColors.accentColor),
-                      ),
-                    ],
-                  ),
+          Expanded(
+            // To make the GridView take available vertical space
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
                 ),
-              ),
-              Card(
-                color: AppColors.primaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Text(
-                        "Party Revenue\nReport",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: AppColors.accentColor),
-                      ),
-                    ],
+                children: [
+                  ReportTilteCard(
+                    title: "Truck Revenue",
+                    icon: Icons.library_books_rounded,
                   ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Card(
-                color: AppColors.primaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Text(
-                        "Party Balance\nReport",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: AppColors.accentColor),
-                      ),
-                    ],
+                  ReportTilteCard(
+                    title: "Party Revenue",
+                    icon: Icons.report,
                   ),
-                ),
-              ),
-              Card(
-                color: AppColors.primaryColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Text(
-                        "Driver Balance\nReport",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: AppColors.accentColor),
-                      ),
-                    ],
+                  ReportTilteCard(
+                    title: "Party Balance",
+                    icon: Icons.report,
                   ),
-                ),
+                  ReportTilteCard(
+                    title: "Supplier Balance",
+                    icon: Icons.report,
+                  ),
+                  ReportTilteCard(
+                    title: "Transaction Report",
+                    icon: Icons.report,
+                  ),
+                  // ReportTilteCard(title: ""),
+                ],
               ),
-            ],
+            ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ReportTilteCard extends StatelessWidget {
+  const ReportTilteCard({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      // color: const Color.fromARGB(255, 255, 251, 230),
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          children: [
+            Icon(
+              icon,
+              size: 30,
+              color: AppColors.primaryColor,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: AppColors.secondaryColor, fontSize: 18),
+            ),
+          ],
+        ),
       ),
     );
   }
